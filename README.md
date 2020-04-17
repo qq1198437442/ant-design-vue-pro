d415e8ad5247034eeac5533e74db44bc42658791
王 志毅 <10167205@qdtre.local>
2020-04-17 13:40:54 +0800

    自定义webpack和babel配置

--- README.md
+++ README.md
@@ -27,3 +27,6 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Description
+It's my antd-vue-pro test project.
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
+// import Button from "ant-design-vue/lib/button";
+// import "ant-design-vue/lib/button/style";
+import { Button } from "ant-design-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+// Vue.use(Antd);
+Vue.use(Button);
 
 new Vue({
   router,

a512d00edad81cd651dce1f67b144db435a68abe
王 志毅 <10167205@qdtre.local>
2020-04-22 14:50:25 +0800

    更加精细化的权限设计(权限组件、权限指令)

--- src/main.js
+++ src/main.js
@@ -8,11 +8,15 @@ import "../antd.conf";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Authorized from "./components/Authorized";
+import Auth from "./directives/auth";
 
 Vue.config.productionTip = false;
 
 // Vue.use(Antd);
 // Vue.use(Button);
+Vue.component("Authorized", Authorized);
+Vue.use(Auth);
 
 new Vue({
   router,

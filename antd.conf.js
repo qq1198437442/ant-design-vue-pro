30a5bd6f8d69df189b86f7b84109dbb54e55a501
王 志毅 <10167205@qdtre.local>
2020-04-24 14:06:10 +0800

    初始数据、自动校验、动态赋值。

--- antd.conf.js
+++ antd.conf.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
-import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
+import {
+  Button,
+  Layout,
+  Icon,
+  Drawer,
+  Radio,
+  Menu,
+  Form,
+  Input
+} from "ant-design-vue";
 Vue.use(Button);
 Vue.use(Layout);
 Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
+Vue.use(Form);
+Vue.use(Input);

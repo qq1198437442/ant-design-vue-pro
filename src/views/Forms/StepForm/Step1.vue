16746510b57b60a8bf57cc77c21bce91875a4a39
王 志毅 <10167205@qdtre.local>
2020-04-25 16:01:37 +0800

    创建一个分部表单

--- src/views/Forms/StepForm/Step1.vue
+++ src/views/Forms/StepForm/Step1.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>第一步</div>
+  <div>
+    <a-form layout="horizontal" :form="form">
+      <a-form-item
+        label="付款账户"
+        :label-col="formItemLayout.labelCol"
+        :wrapper-col="formItemLayout.wrapperCol"
+      >
+        <a-input
+          v-decorator="[
+            'payAccount',
+            {
+              initialValue: step.payAccount,
+              rules: [{ required: true, message: '请输入付款账号' }]
+            }
+          ]"
+          placeholder="请输入付款账号"
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" @click="handleSubmit">下一步</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "",
   data() {
-    return {};
+    this.form = this.$form.createForm(this);
+    return {
+      formItemLayout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 4 }
+      },
+      payAccount: ""
+    };
+  },
+  computed: {
+    step() {
+      return this.$store.state.form.step;
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const { form, $router, $store } = this;
+      form.validateFields((err, values) => {
+        if (!err) {
+          $store.commit({
+            type: "form/saveStepFormData",
+            payload: values
+          });
+          $router.push("/form/step-form/confirm");
+        }
+      });
+    }
   }
 };
 </script>
 
-<style lang="" scoped></style>
+<style scoped></style>
